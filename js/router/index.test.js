b6ad86bd291041b44f4d452ad7a796c2190b6a30
Driss KOHKOH <kohkoh.driss@gmail.com>
2023-03-04 11:32:27 +0100

    branch main test

--- js/router/index.test.js
+++ js/router/index.test.js
@@ -2,68 +2,64 @@
  * @jest-environment jsdom
  */
 
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
 
-import {
-    getByTestId
-} from '@testing-library/dom'
+import { getByTestId } from "@testing-library/dom";
 
-import { router } from './index'
+import { router } from "./index";
 
-
-describe('Router Integration Test Suites', () => {
-    it('should render the sign in page', async () => {
-
-        document.body.innerHTML = `
+describe("Router Integration Test Suites", () => {
+  it("should render the sign in page", async () => {
+    document.body.innerHTML = `
             <div id="root"></div>
-        `
+        `;
 
-        await router()
+    await router();
 
-        expect(
-            getByTestId(document.body, 'sign-in-form-title')
-        ).toHaveTextContent('Veuillez vous connecter')
-    })
+    expect(getByTestId(document.body, "sign-in-form-title")).toHaveTextContent(
+      "Veuillez vous connecter"
+    );
+  });
 
-    it('should render the sensors home page', async () => {
-        document.body.innerHTML = `
+  it("should render the sensors home page", async () => {
+    document.body.innerHTML = `
             <div id="root"></div>
-        `
+        `;
 
-        document.location = '/#/home'
+    document.location = "/#/home";
 
-        await router()
+    await router();
 
-        expect(
-            getByTestId(document.body, 'home-sensors-title')
-        ).toHaveTextContent('Vos capteurs')
-    })
+    expect(getByTestId(document.body, "home-sensors-title")).toHaveTextContent(
+      "Vos capteurs"
+    );
+  });
 
-    it('should render the sensor page', async () => {
-        document.body.innerHTML = `
+  it("should render the sensor page", async () => {
+    document.body.innerHTML = `
             <div id="root"></div>
-        `
+        `;
 
-        document.location = '/#/facade-details'
+    document.location = "/#/facade-details";
 
-        await router()
+    await router();
 
-        expect(
-            getByTestId(document.body, 'sensor-detail-title')
-        ).toHaveTextContent('Détails du capteur #7')
-    })
-    
-    it('should render the add sensor page', async () => {
-        document.body.innerHTML = `
+    expect(getByTestId(document.body, "sensor-detail-title")).toHaveTextContent(
+      "Détails du capteur #7"
+    );
+  });
+
+  it("should render the add sensor page", async () => {
+    document.body.innerHTML = `
             <div id="root"></div>
-        `
+        `;
 
-        document.location = '/#/add-sensor'
+    document.location = "/#/add-sensor";
 
-        await router()
+    await router();
 
-        expect(
-            getByTestId(document.body, 'add-sensor-title')
-        ).toHaveTextContent("Ajout d'un nouveau capteur")
-    })
-})
+    expect(getByTestId(document.body, "add-sensor-title")).toHaveTextContent(
+      "Ajout d'un nouveau capteur"
+    );
+  });
+});
